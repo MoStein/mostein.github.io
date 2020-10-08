3d9560f52cf3fe58ba97e4e5e5af5e9d276d5c16
Mo Stein <moritz.jakobstein@hs-furtwangen.de>
2020-10-08 14:14:02 +0200

    Update Popup.ts

--- EIAzwei/L00_PopUp/Popup.ts
+++ EIAzwei/L00_PopUp/Popup.ts
@@ -3,7 +3,7 @@ namespace Popup{
 let person = prompt("Trag deinen Namen bitte ein");
 
     if (person != null) {
-        document.getElementById("zeug").innerHTML="Hallo" + person + "Willkommen auf dieser Seite."
+        document.getElementById("zeug").innerHTML="Hallo " + person + " Willkommen auf dieser Seite."
     }
 
 }
