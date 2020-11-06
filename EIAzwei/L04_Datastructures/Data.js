82b6350463d902a12581c7784ddf81c94e95dea7
Mo Stein <moritz.jakobstein@hs-furtwangen.de>
2020-11-06 19:19:12 +0100

    Create Data.js

---
+++ EIAzwei/L04_Datastructures/Data.js
@@ -0,0 +1,11 @@
+var L04_hexenkessel;
+(function (L04_hexenkessel) {
+    L04_hexenkessel.data = {
+        zutaten: [
+            { name: "Schlangenzunge", price: 1.30 },
+            { name: "Pferdeauge", price: 0.120 },
+            { name: "Fischdarm", price: 4.99 },
+            { name: "Ganze Spinnen", price: 9.00 }
+        ]
+    };
+})(L04_hexenkessel || (L04_hexenkessel = {}));
