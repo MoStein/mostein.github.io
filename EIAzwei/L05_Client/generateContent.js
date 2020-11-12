be9a9e09df35b94a0897527d2830abe34a65571b
Mo Stein <moritz.jakobstein@hs-furtwangen.de>
2020-11-12 21:40:17 +0100

    Create generateContent.js

---
+++ EIAzwei/L05_Client/generateContent.js
@@ -0,0 +1,35 @@
+var L04_hexenkessel;
+(function (L04_hexenkessel) {
+    function generateContent(_data) {
+        var group = null;
+        for (var category in _data) {
+            var items = _data[category];
+            group.appendChild(createMultiple(items, category));
+        }
+        var fieldset = document.querySelector("fieldset#zutaten");
+        if (fieldset && group) {
+            fieldset.appendChild(group);
+        }
+    }
+    L04_hexenkessel.generateContent = generateContent;
+    function createMultiple(_items, _category) {
+        var group = document.createElement("div");
+        for (var _i = 0, _items_1 = _items; _i < _items_1.length; _i++) {
+            var item = _items_1[_i];
+            var checkbox = document.createElement("input");
+            checkbox.type = "checkbox";
+            checkbox.setAttribute("G", item.priceG.toFixed(2));
+            checkbox.setAttribute("S", item.priceS.toFixed(2));
+            checkbox.setAttribute("K", item.priceK.toFixed(2));
+            checkbox.value = item.name;
+            checkbox.name = _category;
+            checkbox.id = item.name;
+            var label = document.createElement("label");
+            label.textContent = item.name;
+            label.htmlFor = item.name;
+            group.appendChild(checkbox);
+            group.appendChild(label);
+        }
+        return group;
+    }
+})(L04_hexenkessel || (L04_hexenkessel = {}));
