513360424941f31eca9a310499f56c513387537e
MoStein <48826900+MoStein@users.noreply.github.com>
2020-12-04 10:53:05 +0100

    Update Server.js

--- EIAzwei/L07_Database/Server.js
+++ EIAzwei/L07_Database/Server.js
@@ -1,17 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.L04_hexenkessel = void 0;
 const Http = require("http");
 const Url = require("url");
 var L04_hexenkessel;
 (function (L04_hexenkessel) {
-    let server = Http.createServer();
+    let recipes;
     let port = process.env.PORT;
     if (port == undefined)
         port = 5001;
-    console.log("Server starting on port: " + port);
-    server.listen(port);
-    server.addListener("request", handleRequest);
+    let databaseUrl = "mongodb://localhost:27017";
+    startServer(port);
+    connectToDatabase(databaseUrl);
+    function startServer(_port) {
+        let server = Http.createServer();
+        console.log("Server starting on port: " + port);
+        server.listen(port);
+        server.addListener("request", handleRequest);
+    }
+    function connectToDatabase(_url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let options = { useNewUrlParser: true, useUnifiedTopology: true };
+            let mongoClient = new Mongo.MongoClient(_url, options);
+            yield mongoClient.connect();
+            recipes = mongoClient.db("Hexenkessel").collection("Recipes");
+            console.log("Database connection ", recipes != undefined);
+        });
+    }
     function handleRequest(_request, _response) {
         console.log("What's up?");
         _request.setHeader("conten-type", "type/html; charset=utf-8");
@@ -23,8 +47,23 @@ var L04_hexenkessel;
             }
             let jsonString = JSON.stringify(url.query);
             _response.write(jsonString);
+            storeRecipe(url.query);
         }
         _response.end();
     }
+    function handleRetrieveRecipes(_request, _response) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let allRecipes = recipes.find();
+            let allRecipesString = yield allRecipes.toArray();
+            for (let recipe of allRecipesString) {
+                for (let key of allRecipesString) {
+                    _response.write(key + ": " + Object(recipe)[key] + "\n");
+                }
+            }
+        });
+    }
 })(L04_hexenkessel = exports.L04_hexenkessel || (exports.L04_hexenkessel = {}));
+function storeRecipe(_recipe) {
+    recipes.insert(_recipe);
+}
 //# sourceMappingURL=Server.js.map
