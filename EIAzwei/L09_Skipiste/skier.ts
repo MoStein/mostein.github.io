06d11313193148078eee8352a16cb7df61c5c8fa
MoStein <48826900+MoStein@users.noreply.github.com>
2021-01-07 13:25:50 +0100

    Update skier.ts

--- EIAzwei/L09_Skipiste/skier.ts
+++ EIAzwei/L09_Skipiste/skier.ts
@@ -1,60 +1,25 @@
 namespace skipiste {
     export class Skier {
-        x: number;
-        y: number;
-        dx: number;
-        dy: number;
+        position: Vector;
+        
         color: string;
 
-        constructor (_x: number, _y:number, _dx: number, _dy: number, _color: string){
+        constructor (_position: Vector, _size: Vector){
             console.log("constructor skier");
-            this.x = _x;
-            this.y = _y;
-            this.dx = _dx;
-            this.dy = _dy;
-            this.color = _color;
+            
             
         }
         draw (){
             console.log("draw skier");
 
-            crc2.fillStyle = this.color;
-            crc2.fillRect(this.x, this.y, 9, - 25);
-
-            crc2.beginPath();
-            crc2.arc(this.x + 5, this.y - 25, 7, 0, 2 * Math.PI);
-            crc2.fillStyle = this.color;
-            crc2.fill();
-
-            crc2.fillStyle = this.color;
-            crc2.beginPath();
-            crc2.moveTo(this.x + 20, this.y - 4);
-            crc2.lineTo(this.x - 20, this.y + 4);
-            crc2.stroke();
             
         }
         move(){
             console.log("move skier");
 
-            crc2.fillStyle = this.color;
-            crc2.fillRect(this.x, this.y, 9, - 25);
-
-            crc2.beginPath();
-            crc2.arc(this.x + 5, this.y - 25, 7, 0, 2 * Math.PI);
-            crc2.fillStyle = this.color;
-            crc2.fill();
-
-            crc2.fillStyle = this.color;
-            crc2.beginPath();
-            crc2.moveTo(this.x + 20, this.y - 4);
-            crc2.lineTo(this.x - 20, this.y + 4);
-            crc2.stroke();
+           
         }
-        update(): void {
-            console.log("move Skifahrer.ts");
-            this.move();
-            this.draw();
-        }
+        
     
     }
 }
