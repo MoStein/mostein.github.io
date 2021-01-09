bbd1bcec1b3ac5597fa3fa1a62a7606101b53c03
MoStein <48826900+MoStein@users.noreply.github.com>
2021-01-09 15:00:17 +0100

    Create moveable.js

---
+++ EIAzwei/L10_Skipiste/moveable.js
@@ -0,0 +1,30 @@
+var skipiste;
+(function (skipiste) {
+    class Moveable {
+        constructor(_position) {
+            if (_position)
+                this.position = _position.copy;
+            else
+                this.position = new skipiste.Vector(0, 0);
+            this.velocity = new skipiste.Vector(0, 0);
+        }
+        move(_timeslice) {
+            let offset = this.velocity.copy();
+            offset.scale(_timeslice);
+            this.position.add(offset);
+            if (this.position.x < 0)
+                this.position.x += skipiste.crc2.canvas.width;
+            if (this.position.y < 0)
+                this.position.y += skipiste.crc2.canvas.height;
+            if (this.position.x > skipiste.crc2.canvas.width)
+                this.position.x -= skipiste.crc2.canvas.width;
+            if (this.position.y > skipiste.crc2.canvas.height)
+                this.position.y -= skipiste.crc2.canvas.height;
+        }
+        draw() {
+            // leer
+        }
+    }
+    skipiste.Moveable = Moveable;
+})(skipiste || (skipiste = {}));
+//# sourceMappingURL=moveable.js.map
