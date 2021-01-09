79c957d4db4a82feb24e0d33ba1d9de7124e2d46
MoStein <48826900+MoStein@users.noreply.github.com>
2021-01-09 15:00:30 +0100

    Create skier.js

---
+++ EIAzwei/L10_Skipiste/skier.js
@@ -0,0 +1,14 @@
+var skipiste;
+(function (skipiste) {
+    class Skier extends skipiste.Moveable {
+        constructor(_position, _size) {
+            super(_position);
+            console.log("constructor skier");
+        }
+        draw() {
+            console.log("draw skier");
+        }
+    }
+    skipiste.Skier = Skier;
+})(skipiste || (skipiste = {}));
+//# sourceMappingURL=skier.js.map
